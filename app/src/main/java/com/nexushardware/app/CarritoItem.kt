b82962973c9ab53ef0b879e0bd7ece95b96b034d
Miguel Lazo <190323837+Lazzz000@users.noreply.github.com>
2026-02-18 02:07:18 -0500

    test: remplazar fragment Contacto por Carrito y ajustar dataclass CarritoItem

--- app/src/main/java/com/nexushardware/app/CarritoItem.kt
+++ app/src/main/java/com/nexushardware/app/CarritoItem.kt
@@ -1,10 +1,10 @@
 package com.nexushardware.app
 
-data class CarritoItem {
+data class CarritoItem(
     val idCarrito: Int,
     val idProducto: Int,
     val nombre: String,
     val precio: Double,
     val cantidad: Int,
     val urlImagen: String
-}
+)
